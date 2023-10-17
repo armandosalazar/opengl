b410f8b7dcda9155025b86e1d45844f6b502d1af
Armando Salazar <armandosalazar351@gmail.com>
2023-10-16 21:25:28 -0600

    feat: works transladation

--- graphics/src/main.cpp
+++ graphics/src/main.cpp
@@ -93,79 +93,23 @@ void glfwKeyCallback(GLFWwindow *window, int key, int scancode, int action, int 
     }
 }
 
-// void DisplayFunction()
-// {
-//     static int frame = 0;
-//     glPointSize(1.5f);
-
-//     // Calcula las nuevas coordenadas para la traslación
-//     x = 100 * sin(frame * 0.1); // Ajusta el valor 0.1 según la velocidad de traslación deseada
-//     y = 100 * cos(frame * 0.1);
-
-//     // Dibuja las formas trasladadas
-//     // glColor3f(1.0f, 0.0f, 0.0f);
-//     // FillCircleInundation(0, 0, 100);
-//     // glColor3f(0.0f, 0.0f, 1.0f);
-//     // FillRectangleScanline(-50, -50, 50, 50);
-//     // glColor3f(0.0f, 1.0f, 0.0f);
-//     // FillEllipseScanline(0, 0, 50, 20);
-
-//     // Dibuja la forma trasladada con las coordenadas modificadas
-//     glColor3f(1.0f, 1.0f, 1.0f);
-//     FillCircleInundation(x, y, 100);
-
-//     frame++; // Incrementa el contador de frames
-
-//     // Vuelve a dibujar
-//     // glfwSwapBuffers(glfwGetCurrentContext());
-// }
-
-
 void DisplayFunction()
 {
     static int frame = 0;
     glPointSize(1.5f);
 
-    // Dibuja las formas originales en su posición original
-    // glColor3f(1.0f, 0.0f, 0.0f);
-    // FillCircleInundation(0, 0, 100);
-    // glColor3f(0.0f, 0.0f, 1.0f);
-    // FillRectangleScanline(-50, -50, 50, 50);
-    // glColor3f(0.0f, 1.0f, 0.0f);
-    // FillEllipseScanline(0, 0, 50, 20);
-
-    // Aplica la traslación a las coordenadas solo en la dirección X (izquierda a derecha)
-    TranslatePoint(x, y, 1, 0);
+    if (frame % 400 < 200)
+    {
+        x++;
+    }
+    else
+    {
+        x--;
+    }
 
     // Dibuja la forma trasladada con las coordenadas modificadas
     glColor3f(1.0f, 1.0f, 1.0f);
     FillCircleInundation(x, 0, 100);
 
-    // Restringe la traslación en el eje X a un valor máximo (por ejemplo, 200)
-    if (x > 200)
-    {
-        x = -200; // Reinicia la posición en la izquierda cuando llega al límite derecho
-    }
-
     frame++; // Incrementa el contador de frames
-
-    // Vuelve a dibujar
-    // glfwSwapBuffers(glfwGetCurrentContext());
 }
-
-
-// void DisplayFunction()
-// {
-// 	glPointSize(1.5f);
-//     glColor3f(1.0f, 0.0f, 0.0f);
-//     FillCircleInundation(0, 0, 100);
-//     glColor3f(0.0f, 0.0f, 1.0f);
-//     FillRectangleScanline(-50, -50, 50, 50);
-//     glColor3f(0.0f, 1.0f, 0.0f);
-//     FillEllipseScanline(0, 0, 50, 20);
-
-//     // TODO:  use void TranslatePoint(int &x, int &y, int tx, int ty) for translate the shapes
-//     glColor3f(1.0f, 1.0f, 1.0f);
-//     FillCircleInundation(0, 0, 100 + x);
-//     TranslatePoint(x, y, 1, 1);
-// }
